201122ae6352cec4cc0495cc64a7bf5cfc78831d
j v <vantjac@gmail.com>
2024-02-04 15:29:42 -0800

    Save patterns beyond last in sequence

--- src/ModWriter.js
+++ src/ModWriter.js
@@ -29,8 +29,9 @@ function writeModule(module) {
     for (let s = 1; s < numSamples; s++) {
         let offset = s * 30 - 10;
         if (s >= module.samples.length || !module.samples[s]) {
-            // empty sample; just write volume
+            // empty sample
             view.setUint8(offset + 25, maxVolume);
+            view.setUint16(offset + 28, 1);
             continue;
         }
         let sample = module.samples[s];
@@ -52,7 +53,11 @@ function writeModule(module) {
     view.setUint8(951, module.restartPos);
     let seqArr = new Uint8Array(buf, 952, numSongPositions);
     seqArr.set(module.sequence);
-    // TODO: save additional patterns
+    let numPatterns = Math.max(...module.sequence) + 1;
+    if (numPatterns < module.patterns.length && module.sequence.length < numSongPositions) {
+        seqArr[module.sequence.length] = module.patterns.length - 1;
+        numPatterns = module.patterns.length;
+    }
 
     let initials;
     if (module.numChannels == 4) {
@@ -64,9 +69,8 @@ function writeModule(module) {
     }
     writeString(buf, 1080, 4, initials);
 
-    let numPatterns = Math.max(...module.sequence) + 1;
     let patternSize = module.numChannels * numRows * 4;
-    for (let p = 0; p < numPatterns && p < module.patterns.length; p++) {
+    for (let p = 0; p < numPatterns; p++) {
         let pat = module.patterns[p];
         let patOff = 1084 + patternSize * p;
         for (let row = 0; row < numRows; row++) {
