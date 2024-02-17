3cb36cfb2fc84ae964dd376ef9d0c05514e796d0
j v <vantjac@gmail.com>
2024-02-16 21:07:21 -0800

    Add WebApp.js

---
+++ src/ui/WebApp.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Disable pinch to zoom on iOS
+document.addEventListener('touchmove', e => {
+    // @ts-ignore
+    if (e.scale && e.scale != 1)
+        e.preventDefault();
+}, {passive: false});
