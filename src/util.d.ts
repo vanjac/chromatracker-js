96262500083b742a30292b22cb99bd6dcda181d8
j v <vantjac@gmail.com>
2024-03-10 00:52:41 -0800

    d.ts comments

--- src/util.d.ts
+++ src/util.d.ts
@@ -1,14 +1,17 @@
+// Custom overrides for TypeScript's built-in libraries
+
 interface ObjectConstructor {
     // Cleaner return types for Object.assign()
     assign<T extends {}>(target: T, ...sources: Partial<T>[]): T
 }
 
-// These are the only types we care about
+// These are the only form element types we care about when using namedItem()
 type NamedFormItem = HTMLInputElement | RadioNodeList | null
 interface HTMLFormControlsCollection {
     namedItem(name: string): NamedFormItem
 }
 
 interface DocumentFragment {
+    // Overrides Node.cloneNode()
     cloneNode(deep?: boolean): DocumentFragment
 }
