8039f1ea38d00e45cafdc0b661918d45a02e0032
j v <vantjac@gmail.com>
2024-03-31 21:45:53 -0700

    Convert KeyPad to an ES6 class

--- src/ui/KeyPad.js
+++ src/ui/KeyPad.js
@@ -8,59 +8,60 @@ function setupKeypadKeyEvents(elem) {
     elem.addEventListener('touchdown', e => e.preventDefault())
 }
 
-/**
- * @param {Element} container
- * @param {(id: number, elem: Element) => void} onPress
- * @param {(id: number) => void} onRelease
- */
-function KeyPad(container, onPress, onRelease) {
-    this.container = container
-    /** @type {Map<number, Element>} */
-    this.pressed = new Map()
-    this.onPress = onPress
-    this.onRelease = onRelease
-
-    container.addEventListener('mousedown', /** @param {MouseEventInit} e */ e => {
-        if (e.button == 0) {
-            this.press(-1, e.clientX, e.clientY)
-        }
-    })
-    container.addEventListener('mousemove', /** @param {MouseEventInit} e */ e => {
-        if (e.buttons & 1) {
-            this.press(-1, e.clientX, e.clientY)
-        }
-    })
-    container.addEventListener('mouseup', /** @param {MouseEventInit} e */ e => {
-        if (e.button == 0) {
-            this.release(-1)
-        }
-    })
-    container.addEventListener('mouseleave', /** @param {MouseEventInit} e */ e => {
-        if (e.buttons & 1) {
-            this.release(-1)
-        }
-    })
-
+class KeyPad {
     /**
-     * @param {TouchEventInit & Event} e
+     * @param {Element} container
+     * @param {(id: number, elem: Element) => void} onPress
+     * @param {(id: number) => void} onRelease
      */
-    let onTouchDown = e => {
-        for (let touch of e.changedTouches) {
-            if (this.press(touch.identifier, touch.clientX, touch.clientY)) {
-                e.preventDefault()
+    constructor(container, onPress, onRelease) {
+        this.container = container
+        /** @type {Map<number, Element>} */
+        this.pressed = new Map()
+        this.onPress = onPress
+        this.onRelease = onRelease
+
+        container.addEventListener('mousedown', /** @param {MouseEventInit} e */ e => {
+            if (e.button == 0) {
+                this.press(-1, e.clientX, e.clientY)
+            }
+        })
+        container.addEventListener('mousemove', /** @param {MouseEventInit} e */ e => {
+            if (e.buttons & 1) {
+                this.press(-1, e.clientX, e.clientY)
+            }
+        })
+        container.addEventListener('mouseup', /** @param {MouseEventInit} e */ e => {
+            if (e.button == 0) {
+                this.release(-1)
+            }
+        })
+        container.addEventListener('mouseleave', /** @param {MouseEventInit} e */ e => {
+            if (e.buttons & 1) {
+                this.release(-1)
+            }
+        })
+
+        /**
+         * @param {TouchEventInit & Event} e
+         */
+        let onTouchDown = e => {
+            for (let touch of e.changedTouches) {
+                if (this.press(touch.identifier, touch.clientX, touch.clientY)) {
+                    e.preventDefault()
+                }
             }
         }
+        container.addEventListener('touchstart', onTouchDown)
+        container.addEventListener('touchmove', onTouchDown)
+        container.addEventListener('touchend', /** @param {TouchEventInit & Event} e */ e => {
+            e.preventDefault()
+            for (let touch of e.changedTouches) {
+                this.release(touch.identifier)
+            }
+        })
     }
-    container.addEventListener('touchstart', onTouchDown)
-    container.addEventListener('touchmove', onTouchDown)
-    container.addEventListener('touchend', /** @param {TouchEventInit & Event} e */ e => {
-        e.preventDefault()
-        for (let touch of e.changedTouches) {
-            this.release(touch.identifier)
-        }
-    })
-}
-KeyPad.prototype = {
+
     /**
      * @param {number} id
      * @param {number} x
@@ -76,7 +77,7 @@ KeyPad.prototype = {
             }
         }
         return valid
-    },
+    }
 
     /**
      * @param {number} id
@@ -85,5 +86,5 @@ KeyPad.prototype = {
         if (this.pressed.delete(id)) {
             this.onRelease(id)
         }
-    },
+    }
 }
