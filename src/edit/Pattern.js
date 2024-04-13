c96762b4032010104afa74d9d9f657660cd33c24
j v <vantjac@gmail.com>
2024-04-12 20:44:55 -0700

    Edit functions for pattern copy/paste

--- src/edit/Pattern.js
+++ src/edit/Pattern.js
@@ -49,6 +49,18 @@ function editClonePattern(module, pat) {
 
 /**
  * @param {Readonly<Pattern>} pattern
+ * @param {number} cStart
+ * @param {number} cEnd
+ * @param {number} rStart
+ * @param {number} rEnd
+ */
+function patternSlice(pattern, cStart, cEnd, rStart, rEnd) {
+    return Object.freeze(pattern.slice(cStart, cEnd).map(
+        channel => Object.freeze(channel.slice(rStart, rEnd))))
+}
+
+/**
+ * @param {Readonly<Pattern>} pattern
  * @param {number} c
  * @param {number} r
  * @param {Readonly<Cell>} cell
@@ -58,6 +70,47 @@ function editPatternPutCell(pattern, c, r, cell, parts) {
     return changeItem(pattern, c, channel =>
         changeItem(channel, r, dest =>
             Object.freeze(cellApply(dest, cell, parts))))
+}
+
+/**
+ * @param {Readonly<Pattern>} dest
+ * @param {number} cStart
+ * @param {number} rStart
+ * @param {Readonly<Pattern>} src
+ * @param {CellPart} parts
+ */
+function editPatternWrite(dest, cStart, rStart, src, parts) {
+    let mutPat = [...dest]
+    for (let c = 0; c < src.length; c++) {
+        let srcChan = src[c]
+        let mutChan = [...mutPat[c + cStart]]
+        for (let r = 0; r < srcChan.length; r++) {
+            mutChan[r + rStart] = Object.freeze(cellApply(mutChan[r + rStart], srcChan[r], parts))
+        }
+        mutPat[c + cStart] = Object.freeze(mutChan)
+    }
+    return Object.freeze(mutPat)
+}
+
+/**
+ * @param {Readonly<Pattern>} pattern
+ * @param {number} cStart
+ * @param {number} cEnd
+ * @param {number} rStart
+ * @param {number} rEnd
+ * @param {Readonly<Cell>} cell
+ * @param {CellPart} parts
+ */
+function editPatternFill(pattern, cStart, cEnd, rStart, rEnd, cell, parts) {
+    let mutPat = [...pattern]
+    for (let c = cStart; c < cEnd; c++) {
+        let mutChan = [...mutPat[c]]
+        for (let r = rStart; r < rEnd; r++) {
+            mutChan[r] = Object.freeze(cellApply(mutChan[r], cell, parts))
+        }
+        mutPat[c] = Object.freeze(mutChan)
+    }
+    return Object.freeze(mutPat)
 }
 
 /**
