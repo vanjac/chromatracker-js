4e47f7f2cb28e8757ba03c4b63160d266671a11b
j v <vantjac@gmail.com>
2025-01-14 17:55:18 -0800

    Create download link when needed

--- src/file/External.js
+++ src/file/External.js
@@ -1,16 +1,14 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/File_API/Using_files_from_web_applications
 
-/** @type {HTMLAnchorElement} */
-const downloadLink = document.querySelector('#downloadLink')
-
 /**
  * @param {Blob} blob
  * @param {string} name
  */
 export function download(blob, name) {
     let url = URL.createObjectURL(blob)
-    downloadLink.href = url
-    downloadLink.download = name
-    downloadLink.click()
+    let link = document.createElement('a')
+    link.href = url
+    link.download = name
+    link.click()
     URL.revokeObjectURL(url)
 }
