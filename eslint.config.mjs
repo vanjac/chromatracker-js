b6a0d07648909bc5f0492aee0300136e0cb5c297
j <chroma@chroma.zone>
2025-07-21 17:38:19 -0700

    Upgrade ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import {defineConfig} from 'eslint/config'
+import js from '@eslint/js'
+
+export default defineConfig([{
+    plugins: {js},
+    extends: ["js/recommended"],
+    languageOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+    },
+    rules: {
+        "no-var": "error",
+        "curly": "error",
+        "strict": ["error", "global"],
+        "no-restricted-globals": ["error", "length"],
+        // Deprecated stylistic rules (TODO)
+        "semi": ["error", "never", {"beforeStatementContinuationChars": "always"}],
+        "no-extra-semi": "off",
+        // TypeScript handles these:
+        "no-undef": "off",
+        "no-unused-vars": "off",
+        // annoying:
+        "no-empty": "off",
+        "no-constant-condition": "off",
+    },
+}])
