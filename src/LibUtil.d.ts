1e5bb71c85568f7da8c62b816dc36d8d664468c5
j <chroma@chroma.zone>
2025-08-01 16:36:15 -0700

    Remove alternate Object.assign definition

--- src/LibUtil.d.ts
+++ src/LibUtil.d.ts
@@ -6,11 +6,6 @@ interface ImportMeta {
     main: boolean
 }
 
-interface ObjectConstructor {
-    // Cleaner return types for Object.assign()
-    assign<T extends {}>(target: T, ...sources: Partial<T>[]): T
-}
-
 interface Array<T> {
     // https://github.com/microsoft/TypeScript/issues/31785#issuecomment-948012321
     fill<U>(value: U): Array<U>;
