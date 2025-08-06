09ec385e1615b3a9246291415228d4eca0820cfa
j <chroma@chroma.zone>
2025-08-06 13:47:52 -0700

    Add reloadCSS() function for development

--- src/scratch.js
+++ src/scratch.js
@@ -1,3 +1,22 @@
 export default null
 
 window.location.href = import.meta.resolve('../webl.html')
+
+async function reloadCSS() {
+    /** @type {NodeListOf<HTMLLinkElement>} */
+    let links = document.querySelectorAll('link[rel="stylesheet"]')
+    for (let link of links) {
+        if (!link.dataset.noreload) {
+            let style = document.head.appendChild(document.createElement('style'))
+            style.dataset.href = link.href
+            link.remove()
+        }
+    }
+
+    let styles = document.querySelectorAll('style')
+    for (let style of styles) {
+        if (style.dataset.href) {
+            style.textContent = await fetch(style.dataset.href).then(r => r.text())
+        }
+    }
+}
