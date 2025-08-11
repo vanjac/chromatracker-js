859fee03cac4aa1756102b1a44df5c5d12b99c2a
j <chroma@chroma.zone>
2025-08-10 22:23:33 -0700

    Fix pasting empty clipboard

--- src/ui/GlobalState.js
+++ src/ui/GlobalState.js
@@ -1,10 +1,11 @@
 import {emptyArray, Pattern} from '../Model.js'
+import {freeze} from '../Util.js'
 
 export default {
     /** @type {Readonly<Int8Array>} */
     audioClipboard: new Int8Array(),
     /** @type {Readonly<Pattern>} */
-    patternClipboard: emptyArray,
+    patternClipboard: freeze([emptyArray]),
     /** @type {Record<string, string>} */
     effectFormData: Object.create(null),
     // Effect parameters
